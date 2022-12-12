9f255262f7f652bab7331b61b2d3c3818cb7168b
Luiz Sena <sena.reve@hotmail.co.uk>
2022-12-11 22:10:37 -0300

    Add MAUI project
    we will use this to develop the UI

---
+++ src/CAT.GUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CAT.GUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/CAT.GUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CAT.GUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
